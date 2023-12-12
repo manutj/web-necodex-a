e6d98389820d7093deed79aa5625dc98a3f181b3
manutj <manuel.tejada95@outlook.com>
2023-12-12 04:01:19 +0000

    if-else y ternarios

--- js/object-conditionals.js
+++ js/object-conditionals.js
@@ -9,6 +9,9 @@ const persona = {
     }
 }
 
+
+
+
 //ACCEDER A LAS PROPIEDADES DEL OBJETO MEDIANTE EL PUNTO O LOS CORCHETES
 
 /* 
@@ -22,18 +25,18 @@ const persona = {
 
 
 
-// DECLARAR UN OBJETO CONSTRUIDO
-// function Persona(nombre, edad, sexo, pasatiempos) {
-// this.nombre = nombre
-// this.edad = edad
-// this.sexo = sexo
-// this.pasatiempos = pasatiempos
-// this.hablar = function () {
-//     return `Hola, soy ${this.nombre} y tengo ${this.edad} años`
-// }
-// }
+//DECLARAR UN OBJETO CONSTRUIDO
+function Persona(nombre, edad, sexo, pasatiempos) {
+this.nombre = nombre
+this.edad = edad
+this.sexo = sexo
+this.pasatiempos = pasatiempos
+this.hablar = function () {
+    return `Hola, soy ${this.nombre} y tengo ${this.edad} años`
+}
+}
 
-// const manuel = new Persona('manuel', 27, 'masculino', ['patinar', 'bailar'])
+const manuel = new Persona('manuel', 27, 'masculino', ['patinar', 'bailar'])
 
 
 
@@ -51,10 +54,77 @@ const myFunc = myObj.nombre // guarda la referencia de lo que tiene el nombre en
 
 myObj.nombre = null // actualiza el valor del nombre
 
-console.log(myObj.nombre) //Muestra el valor del nombre actualizado
+//console.log(myObj.nombre) //Muestra el valor del nombre actualizado
 
-console.log(myFunc) //Muestra el valor de la referencia que tuvo el nombre antes de la actualizacion
+//console.log(myFunc) //Muestra el valor de la referencia que tuvo el nombre antes de la actualizacion
 
 
+
+
+/* ********************************* CONDICIONALES ****************************** */
+
+/* if (10 > 5) console.log('Es mayor') // EJECUTAR UNA SOLA INSTRUCCION SI LA CONDICION ES VERDADERA
+
+
+
+// SI LA PRIMERA CONDICION ES VERDADERA, EJECUTA EL CODIGO PARA ESA CONDICION Y TERMINA EL IF; SINO EJECUTA EL CODIGO DEL ELSE Y TERMINA EL IF
+
+ if (10 > 5) {
+     console.log('Es mayor')
+    prompt('Escribe tu edad')
+ } else {
+     console.log('Es menor')
+ }
+
+
+ if(10>11){
+     console.log('mensaje 1')
+ } else if(10>20){
+     console.log('mensaje 2')
+ } else if (10 < 7) {
+     console.log('mensaje 3')
+ } else {
+     console.log('no se cumplio nada de lo anterior')
+ }
+
+
+ if(10 > 5){
+     if(8*4 == 32){
+         console.log('operacion exitosa')
+     }else if ((10 / 2) == 5){
+         console.log('operacion exitosa') 
+     }else{
+         console.log('hubo un problema con la division') 
+     }
+ } else {
+     console.log('hubo un problema con el primer if') 
+ }
+
+ */
+
+
+
+
+/* *********************************** OPERADORES TERNARIOS ***************************************** */
+
+//LOS OPERADORES TERNARIOS SON OTRA FORMA DE EVALUAR CONDICIONES COMO LOS IF-ELSE, SOLO QUE UTILIZAMOS EL SIMBOLO "?" PARA EJECUTAR CODIGO EN CASO LA CONDICION SEA VERDADERA Y EL SIMBOLO ":" PARA EJECUTAR EL CODIGO EN CASO LA CONDICION SE FALSA
+
+/*
+
+ let nota = 7
+ nota < 5 ? console.log('nota insuficiente') : console.log('nota suficiente')
+
+let nota = 7
+let calificacion =
+
+    nota < 5 ? 'Insuficiente' :
+    nota < 6 ? 'Suficiente' :
+    nota < 8 ? 'Muy bien' :
+    nota <= 9 ? 'Excelente' :
+    'Sobresaliente'
+
+    console.log('He obtenido un', calificacion)
+
+*/
 
 
