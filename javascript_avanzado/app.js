65f14afb6145b6c0bb0765ba4d554f3bf81aa64c
manutj <manuel.tejada95@outlook.com>
2024-02-06 21:50:37 -0600

    Event Loop y Callbacks

---
+++ javascript_avanzado/app.js
@@ -0,0 +1,66 @@
+//JAVASCRIPT RUNTIME
+
+//thread ==> Hilo de ejecucion principal, en Javascript solo puede ejecutarse una cosa a la vez 
+
+
+// Callstack => pila de ejecucion de tareas
+
+// Callback queue => cola de ejecucion de tareas callback que pueden demorarse en tener una respuesta
+
+// Web api => metodos y propiedades que pertenecen al navegador que para javascript tambien son asincronas y son enviadas al callback queue al momento de ejecutarse
+
+//La union de estas partes forman el runtime de javascript, a este flujo entre codigo sincrono y asincrono se le conoce como Event Loop
+
+
+console.log("1") // Se envia al callstack y se ejecuta sincronicamente
+console.log("2") // Se envia al callstack y se ejecuta sincronicamente
+setTimeout(()=>{
+    console.log("3")  // Se envia al callstack, pero al ser asincrono lo envia al callback queue y espera una respuesta, lo delega a un thread o hilo de ejecucion diferente, para no detener el thread principal
+}, 3000)
+console.log("4") // Se envia al callstack y se ejecuta sincronicamente
+
+//EL Event Loop, revisa el callstack y el queue y unicamente si el callstack ya esta vacio, va a verificar si hay una respuesta en el callback queue, si la hay, la devuelve y si no, sigue esperando la respuesta
+
+
+
+console.log("1")
+setTimeout(()=>{
+    console.log("2")
+}, 1000)
+//for (let i = 0; i < 99999999; i++) // Tener cuidado de no bloquear al callstack con tareas muy pesadas
+console.log("3")
+
+
+
+const ejemploCallback = (otraFuncion) => {
+    otraFuncion(console.log('Multiplicacion de dos numeros'))
+}
+
+ejemploCallback(()=>{
+    console.log(3*3)
+})
+
+
+let usuarios = [{nombre:'Daniel', edad:30}, {nombre:'Manuel', edad:25}, {nombre:'Kevin', edad:17},{nombre:'Glenda', edad:21},{nombre:'Alan', edad:16}]
+
+
+let registarUsuarios = (usuarios, callbackSuccess, callbackError)=>{
+for (let index = 0; index < usuarios.length; index++) {
+    if(usuarios[index].edad > 18){
+        callbackSuccess(usuarios[index])
+    }else{
+        callbackError(usuarios[index])
+    }
+}
+}
+
+let exito = (usuario) => {
+    console.log(`Registro de ${usuario.nombre} exitoso`)
+}
+
+let error = (usuario) => {
+    console.log(`Error, no pudimos registrar a ${usuario.nombre}`)
+}
+
+
+registarUsuarios(usuarios, exito, error)
