5f6b6f1a405380f2c9149ecc9d9fe7258f167f23
manutj <manuel.tejada95@outlook.com>
2024-04-08 21:30:43 -0600

    JSS Styles y Styled Components

--- intro_frontend/react-app/mi-app-react-vite/src/components/InlineStyle.jsx
+++ intro_frontend/react-app/mi-app-react-vite/src/components/InlineStyle.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import '../inline.css'
+import {createUseStyles} from 'react-jss'
+import styled from 'styled-components'
+
+const styles = createUseStyles({
+  paragraph:{
+    color:'red',
+  },
+  button:{backgroundColor:'red'}
+})
+
 
 const InlineStyle = () => {
-    //PODEMOS DECALARAR LOS ESTILOS DENTRO DE UN OBJETO PARA LUEGO USARLOS EN CUALQUIER ELEMENTO DEL RETURN
 
+  const classes = styles()
+
+  const Myh1 = styled.h1`
+  color:red;
+  background-color: papayawhip;
+  `;
+
+    //PODEMOS DECALARAR LOS ESTILOS DENTRO DE UN OBJETO PARA LUEGO USARLOS EN CUALQUIER ELEMENTO DEL RETURN
     const bandera = false
     const pStyle={
         color:bandera ? 'green' : 'yellow', // ESTILOS CONDICIONALES
@@ -22,6 +39,10 @@ const InlineStyle = () => {
         backgroundColor:'aqua'
         }}>
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi atque quasi porro non hic dolorum molestiae et totam? Incidunt, ratione maxime molestiae deleniti accusamus totam at temporibus. Delectus, reiciendis eaque!</p>
+
+        <p className={classes.paragraph}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, amet iusto. Quam est iusto nam voluptas quasi maiores provident itaque aperiam sequi reiciendis, totam, labore fugit doloribus numquam ipsa adipisci facere culpa laboriosam, architecto inventore asperiores voluptatibus. Quisquam blanditiis asperiores iusto totam dolor? Quisquam est nihil tempore accusantium qui deleniti!</p>
+        <button className={classes.button}>Mi boton</button>
+        <Myh1>Hola desde styled components</Myh1>
     </div>
   )
 }
--- intro_frontend/react-app/mi-app-react-vite/src/components/PokemonInfo.jsx
+++ intro_frontend/react-app/mi-app-react-vite/src/components/PokemonInfo.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react"
 
+// ASIGNAMOS UN VALOR POR DEFECTO AL PROP NAME EN CASO NO SE ENVIE NINGUN VALOR PARA ESTE EN EL COMPONENETE APP.JSX
 const PokemonInfo = ({name = 'ditto'}) => {
 
     const [pokemonInfo, setPokemonInfo] = useState(null)
@@ -11,9 +12,10 @@ const PokemonInfo = ({name = 'ditto'}) => {
         .then(pokemon=>setPokemonInfo(pokemon))
         .then(console.log(pokemonInfo))
       },2000)
-    }, [name])
+    }, [name]) //ESTAS INSTRUCCIONES SOLO SE EJECUTARAN CUANDO EL EL VALOR DEL PROP NAME CAMBIE
     
   return (
+    //RENDERIZADO CONDICIONAL DEPENDIENDO SI TRAE INFORMACION EL ESTADO POKEMONINFO
     pokemonInfo ? (
     <span>El ID del pokemon es {pokemonInfo.id} y su nombre es {pokemonInfo.name}</span>
     ) : (
