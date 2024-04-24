275af6fe8781aa3548c228b4b908ab04e90e91a0
manutj <manuel.tejada95@outlook.com>
2024-04-23 20:26:48 -0600

    Reproductor musical pt2

--- frontend_avanzado/react-context/src/components/SongDetail.jsx
+++ frontend_avanzado/react-context/src/components/SongDetail.jsx
@@ -2,8 +2,24 @@ import React from 'react'
 import {useSongContext} from '../context/useSongContext'
 
 const SongDetail = () => {
+  const {selectSong} = useSongContext()
   return (
-    <div>SongDetail</div>
+    <>
+    {
+      selectSong.title ? (
+        <div>
+          <img src={selectSong.img_url} alt="Imagen del album" />
+          <h3>{selectSong.title}</h3>
+          <p>{selectSong.artist}</p>
+          <p>{selectSong.year}</p>
+        </div>
+      ) : (
+        <div>
+          <h2>Selecciona una cancion</h2>
+        </div>
+      )
+    }
+    </>
   )
 }
 
--- frontend_avanzado/react-context/src/components/SongList.jsx
+++ frontend_avanzado/react-context/src/components/SongList.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import {useSongContext} from '../context/useSongContext'
-
+import './songlist.css'
 
 const SongList = () => {
-    const {list, loading, setSelectSong} = useSongContext()
+    const {list, loading, setselectSong} = useSongContext()
   return (
     <div className='container'>
         {loading ? <h2>Cargando...</h2> : list.map((song)=>(
-            <div key={song.id} className='song' onClick={setSelectSong(song)}>
+            <div key={song.id} className='song' onClick={()=>setselectSong(song)}>
                 <h4>{song.title}</h4>
                 <p>{song.artist}</p>
             </div>
