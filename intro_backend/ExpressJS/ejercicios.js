75756ca4115c0298a403a9914363e39c61f0d487
manutj <manuel.tejada95@outlook.com>
2024-05-07 20:31:43 -0600

    ejercicios parte 2

--- intro_backend/ExpressJS/ejercicios.js
+++ intro_backend/ExpressJS/ejercicios.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const request = require('request')
 
 const app = express()
 
@@ -44,7 +45,36 @@ app.get('/api/usuario/:usuario', (req, res)=>{
 })
 
 
-//PENDIENTE EJERCICIO 4
+// 4. Agrega un endpoint '/api/swapi' que responda a una petición de tipo GET con el personaje solicitado de https://swapi.dev/. El cliente debe mandar el número de personaje mediante params. La respuesta del servidor debe lucir algo así
+//             { 'personaje': {
+//             	'name': 'Luke Skywalker',
+//                         ...,
+// } }
+
+
+
+//FUNCION QUE MANDA A LLAMAR A LA SWAPI
+
+//ENDOPINT DE EXPRESS QUE ATRAVES DEL CALLBACK OBTIENE EL ID DE LOS PARAMS Y SE LO PASA LA LA FUNCION DEFINIDA ARRIBA
+
+
+const getPersonajeSwapi = (id) => {
+return new Promise((resolve, reject)=>{
+    request.get(`https://swapi.dev/api/people/${id}/`, (error, response, body)=>{
+        if(response.statusCode === 200){
+            resolve(JSON.parse(body))
+        }else{
+            reject(error)
+        }
+    })
+})
+}
+app.get('/api/swapi/:idPersonaje',(req,res)=>{
+    const {idPersonaje} = req.params
+    getPersonajeSwapi(idPersonaje).then(personajeInfo=>res.status(200).json({personajeInfo}))
+    .catch(error=>res.status(500).json({error}))
+})
+
 
 
 
@@ -55,6 +85,38 @@ app.get('/api/usuario/:usuario', (req, res)=>{
 
 app.put('/api/body', (req,res)=>{
     res.status(200).json({mensaje: 'Usuario actualizado correctamente',usuario: req.body})
+})
+
+
+
+// 6.  Vuelve a hacer el ejercicio 2 pero enviando num1 y num2 desde el body, a través de una petición POST que responda con el status 200
+
+app.post('/api/suma', (req, res)=>{
+    const {num1, num2} = req.body
+
+    const resultado = num1 + num2
+    res.status(200).json({resultado})
+})
+
+
+// 7.  Crea un endpoint para una petición de tipo DELETE donde envíes un ID (un número cualquiera) a través de params.
+
+// app.delete('/api/v1/usuarios/:id', (req,res)=>{
+//     const {id} = req.params
+//     res.json({mensaje:`Usuario ${id} eliminado con exito`})
+// })
+
+// 8. Si el param contiene el ID 3, entonces responde con un status 200 y el mensaje "se ha eliminado el objeto con ID 3", de lo contrario, si envían cualquier otro número como ID, responde con un status 404 y el mensaje "No se encontró el objeto con el ID especificado".
+
+
+app.delete('/api/v1/usuarios/:id', (req,res)=>{
+    const {id} = req.params
+
+    if(id === '3'){
+    res.status(200).json({mensaje:`Usuario ${id} eliminado con exito`})
+    } else {
+        res.status(404).json({mensaje:`Usuario con id ${id} no se encuentra registrado`}) 
+    }
 })
 
 
